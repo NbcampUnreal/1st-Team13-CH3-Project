86781005b64cd997c23ae91c7babfbc40d3b1485
Jeonguk Choi <jwchoi955@gmail.com>
2025-03-04 17:18:12 +0900

    Fix_킬로그 아이콘 삭제

--- ShooterGameProject/Source/ShooterGameProject/Private/KillLogWidget.cpp
+++ ShooterGameProject/Source/ShooterGameProject/Private/KillLogWidget.cpp
@@ -1,8 +1,8 @@
 #include "KillLogWidget.h"
 #include "Components/TextBlock.h"
-#include "Components/Image.h"
+//#include "Components/Image.h"
 
-void UKillLogWidget::UpdateKillLog_Implementation(const FName& Name, const FString& WeaponType)
+void UKillLogWidget::UpdateKillLog_Implementation(const FName& Name)
 {
 	if (KillerName)
 	{
@@ -14,42 +14,42 @@ void UKillLogWidget::UpdateKillLog_Implementation(const FName& Name, const FStri
 		KilledName->SetText(FText::FromName(Name));
 	}
 
-	if (WeaponIcon)
-	{
-		if (WeaponType == "Pistol")
-		{
-			WeaponIconTexture = LoadObject<UTexture2D>(this, TEXT("/Game/UI/Textures/T_Pistol_Icon.T_Pistol_Icon"));
-		}
-		else if (WeaponType == "Rifle")
-		{
-			WeaponIconTexture = LoadObject<UTexture2D>(this, TEXT("/Game/UI/Textures/T_Rifle_Icon.T_Rifle_Icon"));
-		}
-		else if (WeaponType == "Shotgun")
-		{
-			WeaponIconTexture = LoadObject<UTexture2D>(this, TEXT("/Game/UI/Textures/T_Shotgun_Icon.T_Shotgun_Icon"));
-		}
-		else if (WeaponType == "Melee")
-		{
-			WeaponIconTexture = LoadObject<UTexture2D>(this, TEXT("/Game/UI/Textures/T_Melee_Icon.T_Melee_Icon"));
-		}
-		else if (WeaponType == "Grenade")
-		{
-			WeaponIconTexture = LoadObject<UTexture2D>(this, TEXT("/Game/UI/Textures/T_Grenade_Icon.T_Grenade_Icon"));
-		}
+	//if (WeaponIcon)
+	//{
+	//	if (WeaponType == "Pistol")
+	//	{
+	//		WeaponIconTexture = LoadObject<UTexture2D>(this, TEXT("/Game/UI/Textures/T_Pistol_Icon.T_Pistol_Icon"));
+	//	}
+	//	else if (WeaponType == "Rifle")
+	//	{
+	//		WeaponIconTexture = LoadObject<UTexture2D>(this, TEXT("/Game/UI/Textures/T_Rifle_Icon.T_Rifle_Icon"));
+	//	}
+	//	else if (WeaponType == "ShotGun")
+	//	{
+	//		WeaponIconTexture = LoadObject<UTexture2D>(this, TEXT("/Game/UI/Textures/T_Shotgun_Icon.T_Shotgun_Icon"));
+	//	}
+	//	else if (WeaponType == "Melee")
+	//	{
+	//		WeaponIconTexture = LoadObject<UTexture2D>(this, TEXT("/Game/UI/Textures/T_Melee_Icon.T_Melee_Icon"));
+	//	}
+	//	else if (WeaponType == "Grenade")
+	//	{
+	//		WeaponIconTexture = LoadObject<UTexture2D>(this, TEXT("/Game/UI/Textures/T_Grenade_Icon.T_Grenade_Icon"));
+	//	}
 
-		if (WeaponIconTexture)
-		{
-			WeaponIcon->SetBrushFromTexture(WeaponIconTexture);
-		}
-	}
+	//	if (WeaponIconTexture)
+	//	{
+	//		WeaponIcon->SetBrushFromTexture(WeaponIconTexture);
+	//	}
+	//}
 }
 
-void UKillLogWidget::DeleteTexture()
-{
-	if (WeaponIconTexture)
-	{
-		delete WeaponIconTexture;
-		WeaponIconTexture = nullptr;
-	}
-}
+//void UKillLogWidget::DeleteTexture()
+//{
+//	if (WeaponIconTexture)
+//	{
+//		delete WeaponIconTexture;
+//		WeaponIconTexture = nullptr;
+//	}
+//}
 
